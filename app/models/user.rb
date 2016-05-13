fa7a431ace6c869ec01eb30a1c50f4c2c4a39254
Jeremy Patterson <jrmypttrsn@gmail.com>
2016-05-13 00:04:00 -0500

    Added user model

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+	validates :name, presence: true
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+	validates :email, presence: true, length: {maximum: 255}
+	validates :email, uniqueness: true, 
+										format: { with: VALID_EMAIL_REGEX }
+end
